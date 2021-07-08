b72cc9002e753f7667f7f5ba36435d20e41b4a77
tomchingas <tomchingas@gmail.com>
2021-07-08 10:30:20 -0800

    Add postgresql database

--- practiceproject/settings.py
+++ practiceproject/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'practiceproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'practiceproject',
+        'USER': 'tomchingas',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
     }
 }
 
